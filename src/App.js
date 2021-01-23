8750044c6710f02b1119d6ca0b57b5c9d05eb375
Mutasim-Shatil <mutasimshatil@hotmail.com>
2021-01-23 20:28:12 +0800

    header section complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Header />
     </>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+import background from "../../images/bannerbackground.png";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <p>Best food waiting for your belly</p>
+      <div className='search'>
+          <input type="text" placeholder="Search your food"/>
+          <button className="search-btn">Search</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
