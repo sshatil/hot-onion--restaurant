eb40f0b50f8057a9043f2959dc13e88017210458
Mutasim-Shatil <mutasimshatil@hotmail.com>
2021-01-23 18:04:15 +0800

    navbar complete

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import logo from "../../images/logo2.png";
+import "./Navbar.css";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="left-side">
+        <img src={logo} alt="restaurant log" />
+      </div>
+      <div className="right-side">
+        <div className="shopping-logo">
+          <ShoppingCartIcon />
+        </div>
+        <p>Login</p>
+        <button className="sign-up-btn">Sign Up</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
