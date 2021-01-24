def41b0894cb5dbb3f04580d959ff5a8cca2d756
Mutasim-Shatil <mutasimshatil@hotmail.com>
2021-01-25 01:04:50 +0800

    set react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import "./App.css";
-import FoodItems from "./components/FoodItems/FoodItems";
-import Header from "./components/Header/Header";
+
 import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./components/Home/Home";
+import NoMatch from "./components/NoMatch/NoMatch";
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Header />
-      <FoodItems />
-    </>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="*">
+          <NoMatch />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import FoodItems from '../FoodItems/FoodItems';
+import Header from '../Header/Header';
+
+
+const Home = () => {
+    return (
+      <div>
+        <Header />
+        <FoodItems />
+      </div>
+    );
+}
+
+export default Home
