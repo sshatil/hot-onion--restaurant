434a838229e2fc578ebf3020a5331125648bd4dd
sshatil <mutasimshatil@hotmail.com>
2021-11-25 19:07:24 +0800

    add confirm btn

--- src/components/OrderCart/OrderCart.js
+++ src/components/OrderCart/OrderCart.js
@@ -4,23 +4,30 @@ import "./OrderCart.css";
 
 const OrderCart = () => {
   const [orderFood, setOrderFood] = useContext(FoodContext);
-  console.log(orderFood);
+  const totalPrice = orderFood.map((item) => item.totalPrice);
+  const result = totalPrice.reduce((sum, item) => sum + item, 0);
   return (
-    <div className="order-container">
-      <div className="order-food">
-        {orderFood.map((food) => (
-          <div className="food">
-            <div className="img">
-              <img src={food.img} alt="" />
+    <>
+      <div className="order-container">
+        <div className="order-food">
+          {orderFood.map((food) => (
+            <div className="food">
+              <div className="img">
+                <img src={food.img} alt="" />
+              </div>
+              <h3>Food Name: {food.name}</h3>
+              <p>Price: {food.price}</p>
+              <p>Quantity: {food.quantity}</p>
+              <p>Price: {food.totalPrice}</p>
             </div>
-            <h3>Food Name: {food.name}</h3>
-            <p>Price: {food.price}</p>
-            <p>Quantity: {food.quantity}</p>
-            <p>Price: {food.totalPrice}</p>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+      <div className="total-price">
+        <h3>Total Price: {result}$</h3>
+        <button>Confirm Your order</button>
+      </div>
+    </>
   );
 };
 
